d2e9ac0473e333d0b4947ee44d7fd60efaf738c5
sagarlad27 <118250210+sagarlad27@users.noreply.github.com>
2026-03-13 15:25:44 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-netflix-reyaz-112" # Replace with your actual S3 bucket name
+    bucket = "tetris-terraform-state-sagar" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
